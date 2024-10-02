03c4255d742c5d15b1c5b2f0e7e282568e0c6c51
joyeuxclemet87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-02 17:32:45 +0200

    working with user promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,4 +1,4 @@
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({ firstName, lastName });
 }
-console.log(signUpUser('Bob', 'Dylan'));
+// console.log(signUpUser('Bob', 'Dylan'));
