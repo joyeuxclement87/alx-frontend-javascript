248b177ad5d2a270168f74d1f43d2e2b7b315550
joyeuxclemet87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-02 17:10:46 +0200

    working with on all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,11 @@
+// Handle multiple successful promises
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
-  Promise.all(promises).then((resolve) => {
-    console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
-  })
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    })
+
+    .catch(() => console.log('Signup system offline'));
 }
-handleProfileSignup();
