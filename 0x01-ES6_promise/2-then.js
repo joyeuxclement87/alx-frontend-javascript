072688f58cb8b225408d61d20417c34444b76af4
joyeuxclemet87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-02 16:07:29 +0200

    working with then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -6,5 +6,5 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then({ status: 200, body: 'success' })
     .catch(new Error(''))
-    .finally(console.log('Got a response from the AP'));
+    .finally('Got a response from the AP');
 }
