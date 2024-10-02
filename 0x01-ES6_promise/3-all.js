c96ae679b14e4cdf59b0c3279a48b98d769c1985
joyeuxclemet87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-02 17:12:05 +0200

    working with on all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((value) => {
-      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    .then((resolve) => {
+      console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
     })
 
     .catch(() => console.log('Signup system offline'));
