d2f33760988d6a3721c10721dc7a79f77057756e
joyeuxclemet87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-02 16:09:52 +0200

    working with then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,3 @@
-// working tih When the Promise resolves, return an object with the following attributes
-// status: 200 and body: success
-// When the Promise rejects, return an empty Error object
-// For every resolution, log Got a response from the API to the console
 export default function handleResponseFromAPI(promise) {
   return promise
     .then({ status: 200, body: 'success' })
