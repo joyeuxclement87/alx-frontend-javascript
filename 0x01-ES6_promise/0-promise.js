6285c4fad6be4d16845c78375b5fc7a3252735ba
joyeuxclemet87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-02 10:29:50 +0200

    working with update in 0 promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,7 @@
 // working with promises
 export default function getResponseFromAPI() {
-  const working = true;
   return new Promise((resolve, reject) => {
-    if (working) {
-      resolve();
-    } else {
-      reject();
-    }
+    resolve(2);
+    reject(new Error('nothing found'));
   });
 }
