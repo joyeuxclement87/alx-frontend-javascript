1bdbb7c92dfa10e74ccc1a62322034c1e850e8f9
joyeuxclemet87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-03 19:52:37 +0200

    working with classroom

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
 //working with classroom
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
