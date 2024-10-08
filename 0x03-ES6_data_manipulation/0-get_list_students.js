b0ad45a20d249628d6be64599990f62c60a16b7d
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-08 15:56:32 +0200

    working with fix get list of students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,20 @@
-export default function getListStudents(){
-    return  [{
-        id : 1,
-        firstName : "Guillaume",
-        location :"San Francisco",
-    },
-    {
-        id : 2,
-        firstName : "James",
-        location :"columbia",
-    },
-    {
-        id : 5,
-        firstName : "Serena",
-        location :"San Francisco",
-    },
+export default function getListStudents() {
+  return [{
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
 
-    ]
+  ];
 }
 getListStudents();
