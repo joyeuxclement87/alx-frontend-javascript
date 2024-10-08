ce2d1c285b0a4ea8c714611a73a56ac196055b5b
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-08 16:59:29 +0200

    working with get student by locations

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(array, city) {
+    if (!Array.isArray(array)) {
+      return [];
+    }
+    return array.filter((array) => array.location === city);
+  }
+  
